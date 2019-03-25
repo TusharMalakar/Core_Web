81dba89c4730cec90899808196dd85c37b3e9e09
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-03-24 23:05:02 -0400

    commit # 2

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,4 +1,3 @@
-import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from './user.model';
