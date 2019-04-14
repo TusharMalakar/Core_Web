415a6c47300303ccfbf2646f7fce0ee41f21ccc0
Ed1196 <Edwin.Quintuna82@myhunter.cuny.edu>
2019-04-14 03:19:03 -0400

    Implemented table that shows known classes and skills

--- src/app/home/collab-card/collab-card.component.ts
+++ src/app/home/collab-card/collab-card.component.ts
@@ -1,8 +1,11 @@
+import { UserService } from './../../shared/dbAccess/user.service';
 import { CollabModel } from './../../shared/models/collab.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { TableBuilder } from 'src/app/shared/models/tableBuilder.model';
 
-export interface PerioticElement {
-  
+export interface Requirements{
+  skillOrClass: string,
+  type: string
 }
 
 @Component({
@@ -13,29 +16,80 @@ export interface PerioticElement {
 export class CollabCardComponent implements OnInit {
 
   @Input() collabData: CollabModel;
-  xAxis : string[];
-  yAxis : string[];
+  table: Array<TableBuilder> = [];
+  xAxisReq: Array<string> = [];
+  
 
-
-
-  constructor() { 
-    
-  }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.makeTable();
+  }
+
+  /*
+  Function that will build our table. 
+  async: Will allow us to do await functions
+  */
+  async makeTable(){
+    //Will store the list of users.
+    let yAxisUsers : string[];
+
+    //Fuction will stop here until function getAllRequred returns
+    this.xAxisReq = await this.getAllRequired(); 
+    
+    //Will members of the collaboration
+    yAxisUsers  = this.collabData.members;
+    //console.log("Skills Required: " , xAxisReq);
+    
+    //Will loop through all the users and check if they know the required skills and classes
+    for(let y of yAxisUsers){
+      //console.log(x, " is being checked");
+
+        //Will hold a single instance of TableBuilder modle
+        let tableRow = await this.checkIfKnown(y,this.xAxisReq);
+        this.table.push(tableRow);
+        //console.log(tableRow);
+     }
+  }
+
+  //WIll check if a user knows skill or class, from the list of classes and skills that a user knows
+  async checkIfKnown(userName: string, listOfRequired: string[]){
+    let tmp: TableBuilder = null;
+
+    tmp = new TableBuilder(userName);
+
+    let knownByUser;
+   
+    for(let x of listOfRequired){
+      
+      //Will return both skills and classes in a single array
+      await this.userService.getUserSkillsAndClasses(userName).then(function(result){
+        knownByUser = result;
+      });
+      
+      if(knownByUser.includes(x)){
+        //console.log(userName , " knows ", x);
+        tmp.setKnown(x,true);
+        
+      } else {
+        //console.log(userName , " doesn't know ", x);
+        tmp.setKnown(x,false);
+      }
+    }
+    
+    return tmp;
   }
 
   getAllRequired(){
-    let data: string[] = this.collabData.skills.concat(this.collabData.classes);
-    return data;
+    let xAxisReq: Array<string> = [];
+    xAxisReq = (this.collabData.skills).concat(this.collabData.classes);
+
+    return xAxisReq;
   }
 
-  makeTable(){
-    this.xAxis = this.getAllRequired();
-    this.yAxis  = this.collabData.members;
-    
 
-  }
+
+  
+
+
 
 }
