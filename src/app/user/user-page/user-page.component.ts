23d51eed3021e4cffe2ade78359b9ec1ad69578f
Ed1196 <edwinq1496@gmail.com>
2019-04-24 20:18:29 -0400

    Added picture to user page

--- src/app/user/user-page/user-page.component.ts
+++ src/app/user/user-page/user-page.component.ts
@@ -20,6 +20,12 @@ export class UserPageComponent implements OnInit {
   filteredUsers: String[] = [];
   isLoading = false;
 
+  //place holder of image
+  imageToShow: any;
+
+  //default variable for selected file.
+  fileToUpload: File = null;
+
   /*
   TODO: 
     DISPLAY Image
@@ -34,9 +40,46 @@ export class UserPageComponent implements OnInit {
    }
 
   ngOnInit() {
-    
+    this.profilePicture();
     this.classesForm = this.formBuilder.group({
       userInput: null
     })
   }
+
+
+createImageFromBlob(image: Blob) {
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    
+     //image to show is the final place holder of profile picture
+     this.imageToShow = reader.result;
+     return this.imageToShow
+  }, false);
+
+  if (image) {
+     reader.readAsDataURL(image);
+  }
+}
+
+profilePicture(){
+   this.userService.getPicture().subscribe((picture:Blob)=>{
+     this.createImageFromBlob(picture)
+     //console.log(picture)
+   })
+ }
+ //function which you use in (change)-event of your file input tag:
+ handleFileInput(files: FileList) {
+     this.fileToUpload = files.item(0);
+ }
+
+ uploadFileToActivity() {
+   this.userService.uploadProfilePicture(this.fileToUpload).subscribe((data: any)=>{
+     console.log(data)
+   })
+ //   this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
+ //     // do something, if upload success
+ //     }, error => {
+ //       console.log(error);
+ //     });
+}
 }
